9b4f3394b895f914fc05fbbbe1a84812f5dca906
Shubham-coder-cloud <73992220+Shubham-coder-cloud@users.noreply.github.com>
2020-11-21 13:23:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+  var bullet, wall;
+  var speed, weight;
+
+  function setup() {
+    createCanvas(1600,400);
+  speed=random(223,321)
+  weight=random(30,52)
+
+  bullet=createSprite(50, 200, 50, 50);
+  bullet.velocityX = speed;
+
+  wall=createSprite(1500,200,thickness,height/2);
+  wall.shapeColor=(80,80,80);
+
+  thickness=random(22,83)
+
+  }
+
+  function draw() {
+    background("yellow");  
+
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2)
+  {
+    bullet.velocityX=0;
+    var deformation=0.5 * weight * speed* speed/22509;
+    if(deformation>180)
+    {
+      bullet.shapeColor=color(230,230,0);
+    }
+
+    
+    if(deformation<180 && deformation>100) {
+    bullet.shapeColor=color(230,230,0); 
+  }
+    
+    if(deformation<100)
+    {
+      bullet.shapeColor=color(0,255,0);
+    }
+    }
+    if(hasCollided(bullet,wall))
+    {
+      bullet.velocityX=0;
+      var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+      if(damage>10)
+      {
+        wall.shapeColor=color(255,0,0);
+      }
+      if(damage<10)
+      {
+        wall.shapeColor=color(0,255,0);
+      }
+    }
+    drawSprites();
+  }
+
+  function hasCollided(ANY1,ANY2)
+  {
+    bulletRightEdge=bullet.x +bullet.width;
+    wallLeftEdge=wall.x;
+    if(bulletRightEdge>=wallLeftEdge)
+    {
+      return true
+    }
+    return false;
+  }
