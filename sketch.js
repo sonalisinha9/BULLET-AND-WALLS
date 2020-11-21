e5cf4235dd1ae16f9727d65336bf70a869eff1c3
sonalisinha9 <72332990+sonalisinha9@users.noreply.github.com>
2020-11-21 15:04:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-  var bullet, wall;
+  var bullet, thickness,wall;
   var speed, weight;
 
   function setup() {
@@ -19,49 +19,70 @@
   function draw() {
     background("yellow");  
 
-  if(wall.x-bullet.x < (bullet.width+wall.width)/2)
-  {
-    bullet.velocityX=0;
-    var deformation=0.5 * weight * speed* speed/22509;
-    if(deformation>180)
-    {
-      bullet.shapeColor=color(230,230,0);
-    }
+ function setup() {
+  createCanvas(1600, 400);
 
+  speed=random(223,321)
+  weight=random(30,52)
+
+
+  thickness=random(22,83)
+
+
+
+
+  
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
+
+ 
+  	
     
-    if(deformation<180 && deformation>100) {
-    bullet.shapeColor=color(230,230,0); 
-  }
-    
-    if(deformation<100)
-    {
-      bullet.shapeColor=color(0,255,0);
-    }
-    }
-    if(hasCollided(bullet,wall))
-    {
-      bullet.velocityX=0;
-      var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+    wall=createSprite(1200, 200, thickness, height/2);  
 
-      if(damage>10)
-      {
-        wall.shapeColor=color(255,0,0);
-      }
-      if(damage<10)
-      {
-        wall.shapeColor=color(0,255,0);
-      }
-    }
-    drawSprites();
-  }
+    wall.shapeColor=color(230,230,230);
+  //wall.shapeColor=color(80,80,80)
+}
 
-  function hasCollided(ANY1,ANY2)
+
+function draw() {
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
   {
-    bulletRightEdge=bullet.x +bullet.width;
-    wallLeftEdge=wall.x;
-    if(bulletRightEdge>=wallLeftEdge)
-    {
-      return true
-    }
-    return false;
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
+
+	
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
   }
+
+
+  drawSprites();
+ 
+}
+
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
+}
